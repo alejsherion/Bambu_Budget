23adc822c34155c7c2dca1c4f268f3c594eb16a2
Alejo <alejandro5504@hotmail.com>
2025-06-08 00:56:58 -0500

    Initial proyect and Firebase initial configuration

---
+++ environments/environment.ts
@@ -0,0 +1,15 @@
+// cSpell: disable
+// This file could be overwriting while build process using flag `--prod`.
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBe0Tg4xNqgmtmTGxkhEFuzNwLRrCULIeo",
+        authDomain: "accountmanager-47e25.firebaseapp.com",
+        projectId: "accountmanager-47e25",
+        storageBucket: "accountmanager-47e25.firebasestorage.app",
+        messagingSenderId: "408164759950",
+        appId: "1:408164759950:web:af6f2d64ba2e00c6e24cdf",
+        measurementId: "G-DW71DVZCHQ"
+    }
+};
